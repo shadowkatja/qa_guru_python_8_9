a6f297bec946403332ec7adc4635ebdc22590562
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-12 14:05:16 +0300

    add test

---
+++ tests/test_selene.py
@@ -0,0 +1,27 @@
+import allure
+from allure_commons.types import Severity
+from selene import by, be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+@allure.tag("web")
+@allure.severity(Severity.MINOR)
+@allure.label("owner", "e.goldinova")
+@allure.description("Test without allure steps and decorators")
+@allure.feature("Search issue on githib")
+@allure.link('https://github.com', name='Testing')
+def test_find_issue_on_github():
+
+    browser.open("https://github.com")
+
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    s("#query-builder-test").submit()
+
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s("#issues-tab").click()
+
+    s(by.partial_text("#76")).should(be.visible)
+
+    browser.quit()
